68a98c5f5b27674991c9d9f80061ccf3d88a3b4a
Dawid Stach <dstach20@gmail.com>
2022-08-31 11:17:28 +0200

    added theme variables

---
+++ src/app/types/theme.ts
@@ -0,0 +1,28 @@
+export type Light = {
+    text_color: string;
+    items_color: string;
+    background_color: string;
+    input_color: string;
+};
+
+export type Dark = {
+    text_color: string;
+    items_color: string;
+    background_color: string;
+    input_color: string;
+};
+
+export type Typography_Media = {
+    media: {
+        sm: string;
+        md: string;
+        lg: string;
+        xl: string;
+        xxl: string;
+    };
+
+    font_size: {
+        homepage: number;
+        details: number;
+    };
+};
