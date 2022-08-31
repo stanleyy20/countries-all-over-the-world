4c729373bcc8cdc7310c82e0d371c52cafd59d0f
Dawid Stach <dstach20@gmail.com>
2022-08-31 11:19:36 +0200

    added theme provider and light and dark mode switching functions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { Header } from './components/Header';
+import './styles/theme.ts';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import { DarkTheme, LightTheme, Typography } from './styles/theme';
 
 function App() {
-    return <div className='App'>Hello world</div>;
+    const [theme, setTheme] = useState('light');
+
+    const themeToggler = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+    };
+
+    return (
+        <ThemeProvider theme={theme === 'light' ? LightTheme : DarkTheme}>
+            <div className='App'>
+                <ThemeProvider theme={Typography}>
+                    <Header themeToggler={themeToggler} witchModeIsActive={theme} />
+                </ThemeProvider>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
